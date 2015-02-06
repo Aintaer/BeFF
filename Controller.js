5fffc5323d79f4ed5f12ac39c2b12f8ce6807d34
Alex Lee <axal@adobe.com>
2015-02-06 18:03:57 -0500

    Controller: bindViewEvents and with
    Controller.with() is a new static method in order to reduce boilerplate
    of class creation for cases when you only need to change a few things.
    
    _bindViewEvents() is a new prototype method that is used by _initView to
    add bindings for controller specified prerender and postrender hooks.

--- Controller.js
+++ Controller.js
@@ -4,8 +4,9 @@ define([
   'nbd/trait/log',
   'nbd/trait/pubsub',
   'nbd/Controller/Responsive',
-  './View'
-], function($, extend, log, pubsub, Entity, View) {
+  './View',
+  './Model'
+], function($, extend, log, pubsub, Entity, View, Model) {
   'use strict';
 
   function isData(id, data) {
@@ -47,14 +48,70 @@ define([
       if ($view) {
         this._view.trigger('postrender', $view);
       }
+    },
+
+    _initView: function() {
+      this._super.apply(this, arguments);
+      this._bindViewEvents('prerender');
+      this._bindViewEvents('postrender');
+    },
+
+    _bindViewEvents: function(name) {
+      if (!this[name]) { return; }
+
+      var callbacks = this[name];
+      this.listenTo(this._view, name, function() {
+        var args = arguments;
+
+        if (typeof callbacks === 'function') {
+          return callbacks.apply(this, args);
+        }
+
+        if (typeof callbacks === 'string') {
+          return this[callbacks].apply(this, args);
+        }
+
+        // Loop through arrays
+        if (Array.isArray(callbacks)) {
+          return callbacks.forEach(function(fn) {
+            if (typeof fn === 'string') {
+              fn = this[fn];
+            }
+            fn.apply(this, args);
+          }, this);
+        }
+
+        // Loop through objects
+        for (var key in callbacks) {
+          this[key] = (
+            typeof callbacks[key] === 'string' ?
+            this[callbacks[key]] :
+            callbacks[key]
+          ).apply(this, args);
+        }
+      });
     }
   }, {
-    VIEW_CLASS: View
+    MODEL_CLASS: Model,
+    VIEW_CLASS: View,
+    with: function(options) {
+      var klass = this.extend({
+        prerender: options.prerender,
+        postrender: options.postrender
+      });
+
+      klass.VIEW_CLASS = klass.VIEW_CLASS.extend({
+        mustache: options.mustache,
+        partials: options.partials
+      });
+
+      return klass;
+    }
   })
   .mixin(log)
   .mixin({
     get id() {
-      return this._model.id();
+      return this._model.id;
     },
     get data() {
       return this._model.data();
